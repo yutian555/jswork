6bbd149386afacca79391c122022d73925bdf110
yutian555 <1557905242@qq.com>
2019-10-13 13:03:11 +0800

    yutian

--- work4/work4.js
+++ work4/work4.js
@@ -21,7 +21,7 @@ for(let i=1;i<=level;++i){
 let str = '<table border="1">'
 for(let i=1;i<10;++i){
     str +='<tr>'
-    for(var j=1;j<=i;++i){
+    for(var j=1;j<=i;++j){
         //拼接单元格
         str +='<td>' + j + '*' + i + '=' + ( j * i ) + '</td>'
     }
