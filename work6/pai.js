0edd2ecaf3bd1d5b270f9fde7040306b8f84515a
yutian555 <1557905242@qq.com>
2019-10-17 22:58:55 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,31 @@
+function calculate(){
+    let num = document.getElementById('pai').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('pai').value = pi(unm)
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i=1;j=n;++i){
+        sum
+    }
+    return sum
+}
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1){
+        sum
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial()
+        let divisor = oddFactorial()
+
+    }
+    return sum = 2
+}
+calculate()
