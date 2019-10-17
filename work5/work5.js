cfe799b9e25ab71ad6d074dcfe6366a27a3226bb
yutian555 <1557905242@qq.com>
2019-10-17 23:13:37 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,6 +1,16 @@
 str = '<table border="1">'
 for(let i=9;i>0;--i){
     str += '<tr>'
+    for(var j=9;j>0;--j){
+        str += '<td>&nbsp;</td>'
+}
+str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table1').innerHTML = str
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
     for(var j=i;j>0;--j){
         str += '<td>&nbsp;</td>'
 }
